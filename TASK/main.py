7ea3e7da0ad2e6b16efd8922118dda5534ead2c6
Anna <krynaann13@gmail.com>
2022-08-12 12:15:02 +0200

    Getting number of movies

--- TASK/main.py
+++ TASK/main.py
@@ -13,3 +13,12 @@ for line in data:
 # get only unique names of actors
 actor_names = actor_names.split(', ')
 unique_actor_names = set(actor_names)
+
+# output table
+output_table = []
+
+# get the number of movies per actor
+for actor in unique_actor_names:
+    number_movies_per_actor = actor_names.count(actor)
+    if number_movies_per_actor >= 2:
+        output_table.append([actor, number_movies_per_actor])
