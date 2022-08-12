39a72a2dea71b94a2dc8f00a8f07b7036c27d751
Anna <krynaann13@gmail.com>
2022-08-12 11:58:38 +0200

    Getting basic data

---
+++ INTERN/TASK/main.py
@@ -0,0 +1,13 @@
+import json
+
+# read json
+data = json.load(open('data.json'))
+
+# get all actor names from all movies
+actor_names = str()
+for line in data:
+    actor_names += line['stars']
+
+# get only unique names of actors
+actor_names = actor_names.split(', ')
+unique_actor_names = set(actor_names)
