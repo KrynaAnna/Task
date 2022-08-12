43fa5d93f8db54f97d84de4001c854709be6450c
Anna <krynaann13@gmail.com>
2022-08-12 12:15:46 +0200

    Getting average rating

--- TASK/main.py
+++ TASK/main.py
@@ -22,3 +22,16 @@ for actor in unique_actor_names:
     number_movies_per_actor = actor_names.count(actor)
     if number_movies_per_actor >= 2:
         output_table.append([actor, number_movies_per_actor])
+
+# get the average rating of all the movies in which the actor starred
+for row in output_table:
+    rate_per_actor = []
+    for line in data:
+        if row[0] in line['stars'].split(', '):
+            rate_per_actor.append(float(line['rating']))
+    average_rating_per_actor = Decimal(sum(rate_per_actor) / len(rate_per_actor))
+    average_rating_per_actor = str(round(average_rating_per_actor, 2))
+
+    # appending average rating per actor to output table
+    index_of_row = output_table.index(row)
+    output_table[index_of_row].append(average_rating_per_actor)
